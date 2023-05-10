8a315f497bde81bd77cd6f19000815a9e93dc9f3
ThanhHoang15 <thanh.hoang210@gmail.com>
2023-05-10 12:06:40 -0700

    working on frontend routes

--- controllers/frontEndRoutes.js
+++ controllers/frontEndRoutes.js
@@ -1,77 +1,92 @@
 const express = require('express');
 const router = require('express').Router();
-const {Invoice, User, Department, Wholesaler} = require('../models');
+const { Invoice, User, Department, Wholesaler } = require('../models');
 
-router.get("/", async (req,res)=>{
+router.get('/', (req, res) => {
+    res.render('login');
+});
+
+router.get("/invoice", async (req, res) => {
     Invoice.findAll({
-        include:[User]
-    }).then(invData=>{
-        const hbsData = invData.map(inv=>inv.get({plain:true}));
+        include: [User]
+    }).then(invData => {
+        const hbsData = invData.map(inv => inv.get({ plain: true }));
         console.log(hbsData);
-        res.render("home",{
-            allInvoices:hbsData,
+        res.render("logs", {
+            allInvoices: hbsData,
             logged_in: req.session.logged_in
         })
     })
-})
+});
 
-router.get("/invoice/:id", async (req,res)=>{
-    Invoice.findByPk(req.params.id,{
-        include:[User]
-    }).then(invData=>{
-        const hbsData = invData.get({plain:true});
-        hbsData.logged_id=req.session.logged_id
+router.post("/logs", async (req, res) => {
+    Invoice.findAll({
+        include: [User]
+    }).then(invData => {
+        const hbsData = invData.map(inv => inv.get({ plain: true }));
         console.log(hbsData);
-        res.render("singleInvoice",hbsData)
-    })
-})
-
-router.get("/department/invoice/:id", async (req,res)=>{
-    Invoice.findByPk(req.params.id,{
-        include:[Department]
-    }).then(invData=>{
-        const hbsData = invData.get({plain:true});
-        hbsData.logged_id=req.session.logged_id
-        console.log(hbsData);
-        res.render("deptInvoice",hbsData)
-    })
-})
-
-router.get("/login", async (req,res)=>{
-    if(req.session.logged_in){
-        return res.redirect("/home")
-    }
-    res.render("login",{
-        logged_in:req.session.logged_in
-    })
-})
-
-router.get("/signup", async (req,res) => {
-    try {
-      if (req.session.userId) {
-        res.redirect("/dashboard")
-      } else {
-        res.render("signup")
-      }
-    } catch (error) {
-      console.log(error)
-      res.status(500).json(error);
-    }
-  })
-
-router.get("/home", async (req,res)=>{
-    if(!req.session.logged_in){
-        return res.redirect("/login")
-    } else {
-        User.findByPk(req.session.user_id,{
-            include:[Invoice]
-        }).then(userData=>{
-            const hbsData = userData.get({plain:true})
-            console.log(hbsData)
-            hbsData.logged_in=req.session.logged_in;
-            res.render("home",hbsData)
+        res.render("logs", {
+            allInvoices: hbsData,
+            logged_in: req.session.logged_in
         })
+    })
+});
+
+
+router.get("/login", async (req, res) => {
+    if (req.session.logged_in) {
+        return res.redirect("/homepage")
     }
-})
+    res.render("login", {
+        logged_in: req.session.logged_in
+    })
+});
+
+router.post('/login', (req, res) => {
+    const { email, password } = req.body;
+  
+    User.findOne({ where: { email, password } })
+      .then((user) => {
+        if (user) {
+          req.session.userId = user.id; 
+          req.session.logged_in = true;
+          res.redirect('/homepage');
+        } else {
+          res.redirect('/login');
+        }
+      })
+      .catch((err) => {
+        console.error('Please enter a valid username and password', err);
+        res.redirect('/login');
+      });
+});
+
+router.get("/signup", (req, res) => {
+    try {
+        if (req.session.userId) {
+            res.redirect("/homepage")
+        } else {
+            res.render("login")
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json(error);
+    }
+});
+
+router.post('/signup', (req, res) => {
+    const { email, password } = req.body;
+
+    User.create({ email, password })
+        .then(() => {
+            req.session.userId = user.id; 
+            req.session.logged_in = true;
+            res.redirect('/homepage');
+        })
+        .catch((err) => {
+            console.error('Error creating user', err);
+            res.redirect('/signup');
+        });
+});
 
 module.exports = router;
--- seeds/seeds.js
+++ seeds/seeds.js
@@ -18,6 +18,7 @@ const seedDatabase = async ()=>{
       process.exit(0);
   } catch (err){
       console.log(err)
+      process.exit(1);
   }
 }
   
